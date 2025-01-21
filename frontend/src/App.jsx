b0bb698f1d08945286593e79d41b73df5676e98e
Musa Adamu <msmajemusa4@gmail.com>
2025-01-21 13:35:25 +0100

    rscreen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
-import RegisterScreen from './screens/RegisterScreen';
+import RegisterScreen from './screens/registerScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import PrivateRoute from './components/PrivateRoute';
 import CollegeNameBanner from './components/CollegeNameBanner'
